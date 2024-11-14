8fecfcab054d405097b395b4b3331ada24b72b8f
kenya <kenya.banach94@gmail.com>
2024-11-14 11:49:43 -0300

    atualizacao com o hamburguer

---
+++ assets/js/offcanvas.js
@@ -0,0 +1,14 @@
+// sempre quando abrir um novo arquivo js lembrar de colocar no html na ultima linha o endereçamento; 
+
+function abrirNav(){
+    // seleciona o elemento atraves do id nesse caso entao nao precisa do #; 
+    const menu = document.getElementById("offcanvas");
+    
+
+}
+
+
+function fecharNav(){
+
+}
+
