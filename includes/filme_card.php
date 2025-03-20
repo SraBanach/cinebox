7f58072e8b3f8b021889bb368b0eada9e6be1d7a
SraBanach <kenya.banach94@gmail.com>
2025-03-20 08:31:40 -0300

    filme_card conectando imagens do banco

--- includes/filme_card.php
+++ includes/filme_card.php
@@ -1,26 +1,42 @@
-<!-- card modelo do para ser chamado -->
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+ 
+<html lang="pt-br">
+ 
+<?php
+ 
+ 
+ 
+$dsn = 'mysql:dbname=db_cinebox;host=127.0.0.1';
+$user = 'root';
+$password = '';
+ 
+$banco = new PDO($dsn, $user, $password);
+ 
+$select = 'SELECT * FROM tb_filmes';
+ 
+$resultado = $banco->query($select)->fetchAll();
+ 
+//echo '<pre>'; // pre serve para organizar a tela
+//var_dump($resultado);
+?>
+<?php foreach ($resultado as $linha) { ?>
 <div class="col col-3">
-                <figure>
-                    <!-- comeca com figure e logo depois coloca a imagem utilizada e depois comenta em figcapition -->
-                    <img src="./assets/img/poster/avatar-o-caminho-da-agua.png"
-                        alt="Poster do filme Avatar o caminho da agua" class="foto-produto">
-                    <figcaption>
-                        <!-- conteudo do card colocar o que esta logo abaixo da foto; -->
-                        <h4>Avatar o caminho da agua</h4>
-                        <!-- Span é algo generico para textos como se fosse uma div  -->
-                        <span class="preco">R$15,90</span>
-                        <p class="descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
-                            aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum."</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
+                    <figure>
+                        <!-- se começa primeiro a tag  figure para prossegir com o figcapture  -->
+                        <img src="./assets/img/poster/<?php echo $linha['poster'] ?>"
+                            alt="Poster do filme Avatar o caminho da agua" class="foto-produto">
+                        <figcaption>
+                            <!-- conteudo do card colocar o que esta logo abaixo da foto; -->
+                            <h4><?php echo $linha['nome'] ?></h4>
+                            <!-- o span é o elemento de linha, adequado para aplicar estilos aocontrario da div que é um elemento em bloco  -->
+                            <span class="preco"><?php echo $linha['valor_ingresso'] ?></span>
+                            <p class="descricao"><?php echo $linha['descricao'] ?></p>
+                        </figcaption>
+                        <span class="genero">
+                            <label style="<?php echo $linha['nome'] ?>">Fantasia</label>
+                            <label style="background-color: #580259;">Drama</label>
+                            <label style="background-color: #D966BA;">Ficção</label>
+                        </span>
+                    </figure>
+                </div>
+<?php } ?>
